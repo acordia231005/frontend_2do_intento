000970fe7c7f393815252b0690c6b31fc13d3975
23gordo05 <andrescortesdiaz2005@gmail.com>
2026-06-01 22:12:48 +0200

    falta conectar con base de datos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AlumnoComponent } from './component/alumno-component/alumno-component';
+import { ProfesorComponent } from './component/profesor-component/profesor-component';
+import { AsignaturaComponent } from './component/asignatura-component/asignatura-component';
+import { MatriculaComponent } from './component/matricula-component/matricula-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'alumno', component: AlumnoComponent },
+    { path: 'profesor', component: ProfesorComponent },
+    { path: 'asignatura', component: AsignaturaComponent },
+    { path: 'matricula', component: MatriculaComponent }
+];
