0bce8bb370716db5325c46b60fe9786183a1cc0c
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2026-06-09 14:00:08 +0200

    avances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { AsignaturaComponent } from './component/asignatura-component/asignatura
 import { MatriculaComponent } from './component/matricula-component/matricula-component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: ''},
+    { path: 'header', redirectTo: 'header'},
     { path: 'alumno', component: AlumnoComponent },
     { path: 'profesor', component: ProfesorComponent },
     { path: 'asignatura', component: AsignaturaComponent },
