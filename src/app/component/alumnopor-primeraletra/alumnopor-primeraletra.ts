cd9121f141b4448524c227568618858469064814
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2026-06-11 12:52:10 +0200

    endpoint completo

--- src/app/component/alumnopor-primeraletra/alumnopor-primeraletra.ts
+++ src/app/component/alumnopor-primeraletra/alumnopor-primeraletra.ts
@@ -3,6 +3,9 @@ import { Alumno } from '../../model/alumno';
 import { AlumnoService } from '../../services/alumno-service';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatriculaService } from '../../services/matricula-service';
+import { Matricula } from '../../model/matricula';
+import { Asignatura } from '../../model/asignatura';
 
 @Component({
   selector: 'app-alumnopor-primeraletra',
@@ -12,16 +15,32 @@ import { CommonModule } from '@angular/common';
 })
 export class AlumnoporPrimeraletra implements OnInit {
   public alumnos: Alumno[] = [];
+  public matriculas: Matricula[] = [];
+  public asignaturas: Asignatura[] = [];
 
   ngOnInit(): void {
-    this.obtenerAlumnos();
+    this.obtenerAlumnospora();
+    this.obtenerMatriculas();
   }
 
-  constructor(private _alumnoService: AlumnoService) { }
+  constructor(private _alumnoService: AlumnoService, private _matriculaservice: MatriculaService) { }
 
-  obtenerAlumnos() {
+  obtenerAlumnospora() {
     return this._alumnoService.findAll().subscribe(listaAlumnos =>
       this.alumnos = listaAlumnos.filter(a => a.nombre.toLowerCase().startsWith("a"))
     );
   }
+
+  obtenerMatriculas() {
+    return this._matriculaservice.findAll().subscribe(listaMatricula =>
+      this.matriculas = listaMatricula
+    );
+  }
+
+  obtenerDeLosAlumnosQueEmpiezanPorA(alumno: Alumno) {
+    return this.matriculas.filter(m => m.alumno = alumno).map(
+      m => m.asignatura.nombre
+    );
+  }
+
 }
