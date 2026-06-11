3ce0b8e218a5030582e8d52400b6314f008c7f2e
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2026-06-11 12:23:30 +0200

    endpoint de alumno por letra a

---
+++ src/app/component/alumnopor-primeraletra/alumnopor-primeraletra.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnopor-primeraletra',
+  imports: [],
+  templateUrl: './alumnopor-primeraletra.html',
+  styleUrl: './alumnopor-primeraletra.css',
+})
+export class AlumnoporPrimeraletra {}
